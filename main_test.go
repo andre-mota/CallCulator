e7e1cfca66a8c1bbb0c246bcdea23aa6abf16a79
André Mota <andremota3@gmail.com>
2017-05-03 16:26:52 +0100

    Add more tests.

--- main_test.go
+++ main_test.go
@@ -1,23 +1,31 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 const (
 	testdata1 = "data/testdata.csv"
 	testdata2 = "data/testdata2.csv"
 )
 
-// func TestHandleErr(t *testing.T) {
-// 	err := errors.New("Error!!!")
-// 	e := handleErr(err)
-// 	if e != "Error!!!" {
-// 		t.Fatalf("Error on handleErr(err), should be %s, got %s", "Error!!!", err)
-// 	}
-//}
+func TestHandleErr(t *testing.T) {
+	err := errors.New("Error!!!")
+	e := handleErr(err)
+	if e != "Error!!!" {
+		t.Fatalf("Error on handleErr(err), should be %s, got %s", "Error!!!", err)
+	}
+}
 
 func TestString(t *testing.T) {
 	var m Money = 1575
 	if m.String() != "15.75" {
+		t.Fatalf("Error on m.String(), should be %s, got %s", "15.75", m.String())
+	}
+
+	m = -1575
+	if m.String() != "-15.75" {
 		t.Fatalf("Error on m.String(), should be %s, got %s", "15.75", m.String())
 	}
 }
@@ -35,6 +43,16 @@ func TestFileParser(t *testing.T) {
 	_, ok = tCallerCounter["+351215355312"]
 	if !ok {
 		t.Fatalf("Error on FileParser(testdata1), should have key %s", "+351215355312")
+	}
+}
+
+func TestCalcCost(t *testing.T) {
+	if CalcCost(300) != 25 {
+		t.Fatalf("Error on CalcCost(300), result should be %s, got %s", "25", CalcCost(300))
+	}
+
+	if CalcCost(301) != 27 {
+		t.Fatalf("Error on CalcCost(300), result should be %s, got %s", "25", CalcCost(300))
 	}
 }
 
